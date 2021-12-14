e474614f5bc12cd5359a78def779b7fd8c902495
mkuhn <matt.t.kuhn@gmail.com>
2021-12-14 07:04:43 -0700

    day 14 - cleanup

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -1,75 +1,49 @@
 import java.io.File
 
 fun main() {
-    part1("src/main/resources/day14sample.txt")
-    part1("src/main/resources/day14.txt")
-    part2("src/main/resources/day14sample.txt")
-    part2("src/main/resources/day14.txt")
-}
-
-private fun String.applyPairInsertions(pairInsertionMap: Map<String, String>) =
-    windowed(2, 1, true)
-        .joinToString("") { win -> win[0] + (pairInsertionMap[win] ?: "") }
-
-private fun part1(inputFile: String) {
-    val pairInsertionRegex = "(..) -> (.)".toRegex()
-    val pairInsertionMap = File(inputFile).readLines()
-        .drop(2)
-        .mapNotNull { line -> pairInsertionRegex.find(line)?.destructured?.toList() }
-        .associate { lineList -> lineList[0] to lineList[1] }
-    val polymerTemplate = File(inputFile).readLines().first()
-
-    val step = generateSequence(polymerTemplate) { it.applyPairInsertions(pairInsertionMap) }
-        .take(11)
-        .last()
-
-    val charCounts = step.toList().distinct().associateWith { c -> step.count { it == c } }
-    println(charCounts.maxOf { it.value } - charCounts.minOf { it.value })
+    solve("src/main/resources/day14sample.txt", 10)
+    solve("src/main/resources/day14.txt", 10)
+    solve("src/main/resources/day14sample.txt", 40)
+    solve("src/main/resources/day14.txt", 40)
 }
 
 private fun Map<String, Long>.splitByInsertionRules(pairConversionMap: Map<String, List<String>>): Map<String, Long> =
-    this.map { entry ->
-        (pairConversionMap[entry.key]?.associate { it to entry.value })?:error("bad map: $entry")
-    }.consolidateMappedCounts()
+    map { entry -> pairConversionMap[entry.key]!!.associateWith { entry.value } }.sumMappedCounts()
 
-private fun List<Map<String, Long>>.consolidateMappedCounts(): Map<String, Long> {
-    val consolidatedKeys = map { it.keys }.flatten().distinct()
-    return consolidatedKeys.associateWith { key -> this.sumOf { it[key] ?: 0L } }
-}
+private fun List<Map<String, Long>>.sumMappedCounts(): Map<String, Long> =
+    flatMap { it.keys }.distinct().associateWith { key -> this.sumOf { it[key] ?: 0L } }
 
-private fun Map<String, Long>.pairsToSinglesWrong(): Map<String, Long> =
-    this.map { entry ->
-        listOf(entry.key[0].toString() to entry.value, entry.key[1].toString() to entry.value).toMap()
-    }.consolidateMappedCounts()
+/**
+ * Sum only the first char of each key, as the second was duplicated when we split windowed pairs.
+ * Except the last char of the template was not duplicated, add 1 for it.
+ */
+private fun Map<String, Long>.pairsToSingles(template: String): Map<String, Long> =
+    this.toMutableMap().apply{ this[template.last().toString()] = 1 }
+        .map { entry -> mapOf(entry.key[0].toString() to entry.value) }
+        .sumMappedCounts()
 
-private fun Map<String, Long>.pairsToSingles(): Map<String, Long> =
-    this.map { entry ->
-        listOf(entry.key[0].toString() to entry.value).toMap()
-    }.consolidateMappedCounts()
-
-
-private fun part2(inputFile: String) {
-    val pairInsertionRegex = "(..) -> (.)".toRegex()
-    val pairInsertionMap = File(inputFile).readLines()
-        .drop(2)
-        .mapNotNull { line -> pairInsertionRegex.find(line)?.destructured?.toList() }
-        .associate { lineList -> lineList[0] to lineList[1] }
-
-    val pairConversionMap = pairInsertionMap.entries.associate {
-        it.key to listOf(it.key[0]+it.value, it.value+it.key[1])
-    }
-
+private fun solve(inputFile: String, steps: Int) {
     val template = File(inputFile).readLines().first()
-    val pairCountsInTemplate = File(inputFile).readLines().first()
-        .windowed(2)
-        .groupingBy { it }.eachCount()
-        .map { it.key to it.value.toLong() }.toMap()
 
-    val step = generateSequence(pairCountsInTemplate) { it.splitByInsertionRules(pairConversionMap) }
-        .take(41).last()
-        .pairsToSingles()
-        .toMutableMap()
-        .apply { this[template.last().toString()] = this[template.last().toString()]!! + 1 }
+    val pairInsertionRegex = """(?<pair>..) -> (?<insert>.)""".toRegex()
+
+    //parse out the rule, then change it to a mapping of a pair to a list of the two resulting pairs
+    val pairSplitterMap = File(inputFile).readLines()
+        .drop(2)
+        .mapNotNull { line -> pairInsertionRegex.matchEntire(line)?.groups }
+        .map { group -> group["pair"]!!.value to group["insert"]!!.value }
+        .associate { rule -> rule.first to listOf(rule.first[0]+rule.second, rule.second+rule.first[1]) }
+
+    //count how many of each windowed pair we have in the initial template
+    val pairCountsInTemplate = template.windowed(2)
+        .groupingBy { it }
+        .eachCount()
+        .mapValues { it.value.toLong() }
+
+    //iteratively split each pair according to the splitter map and get a new sum, then count distinct chars for those pairs
+    val step = generateSequence(pairCountsInTemplate) { it.splitByInsertionRules(pairSplitterMap) }
+        .take(steps+1).last()
+        .pairsToSingles(template)
 
     println(step.maxOf { it.value } - step.minOf { it.value })
 }
